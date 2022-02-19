15815b6d68bd91259defab134fc3bde2402d12d0
chen82508 <chen82508@gmail.com>
2022-02-19 19:11:34 +0800

    Verify account by email token URL.

--- djangoApiDemo/authentication/views.py
+++ djangoApiDemo/authentication/views.py
@@ -1,12 +1,15 @@
 from .models import User
 from .serializers import RegisterSerializers
 from .utils import Util
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render
 from django.urls import reverse
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+import jwt
+
 
 # Create your views here.
 class RegisterView(generics.GenericAPIView):
@@ -35,6 +38,26 @@ class RegisterView(generics.GenericAPIView):
         Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
 
+
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass
+
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token,
+                                 settings.SECRET_KEY,
+                                 algorithms=['HS256'])
+            user = User.objects.get(id=payload['user_id'])
+
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+
+            return Response({"email": "Successfully activated."},
+                            status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError:
+            return Response({"error": "Activation expired."},
+                            status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError:
+            return Response({"error": "Invalid token."},
+                            status=status.HTTP_400_BAD_REQUEST)
--- djangoApiDemo/djangoApiDemo/settings/development.py
+++ djangoApiDemo/djangoApiDemo/settings/development.py
@@ -5,18 +5,18 @@ SECRET_KEY = config('SECRET_KEY', 'default-django-secret-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('MySQL_USER', default='root'),
-        'PASSWORD': config('MySQL_PASSWORD', default='root'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': config('DB_NAME'),
+    #     'USER': config('MySQL_USER', default='root'),
+    #     'PASSWORD': config('MySQL_PASSWORD', default='root'),
+    #     'HOST': config('DB_HOST', default='localhost'),
+    #     'PORT': config('DB_PORT', default='3306'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 INSTALLED_APPS += [
