e6cf0eda46aa11d20da51a6cbeb3e1d388a44fe6
chen82508 <chen82508@gmail.com>
2022-03-06 01:06:27 +0800

    Unit Testing Authentication

---
+++ djangoApiDemo/authentication/tests/test_setup.py
@@ -0,0 +1,19 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from faker import Faker
+
+
+class TestSetUp(APITestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+        self.register_url = reverse("login")
+        self.user_data = {
+            "email": self.fake.email(),
+            "username": self.fake.email().split("@")[0],
+            "password": self.fake.email(),
+        }
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
